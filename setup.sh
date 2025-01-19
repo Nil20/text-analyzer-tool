859673fcdbdead0bd962976cec5a78c42e0091c8
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-01-19 15:56:39 +0600

    feat: update setup script to check needed ports in use

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
+is_port_in_use() {
+    netstat -tuln | grep -q ":$1 " 
+}
+
+PORTS=(3306 3000)
+
+for PORT in "${PORTS[@]}"; do
+    if is_port_in_use "$PORT"; then
+        echo "Error: Port $PORT is already in use. Please free it and try again."
+        exit 1
+    fi
+done
+
+echo "Ports are free. Proceeding with Docker setup..."
+
 echo "Building Docker images..."
 docker-compose build --no-cache
 
